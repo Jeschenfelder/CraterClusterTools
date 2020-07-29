549dc3f31f9098801fba7ea8df3b1435243cd207
Jonas Eschenfelder <jae4518@ic.ac.uk>
2020-07-29 17:43:38 +0200

    Update conf.py

--- Sphinx/conf.py
+++ Sphinx/conf.py
@@ -12,7 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../CraterClusterTools'))
+sys.path.insert(0, os.path.abspath(os.sep.join((os.curdir,'..'))))
+
 
 
 # -- Project information -----------------------------------------------------
